8e90dda35ddf37b27319dc232800a59475bd5b40
Christian Bortolotti <cbortolo@purdue.edu>
2020-06-11 16:15:13 -0400

    Initialize Project
    -Create vue project
    -Connect bootstrap-vue

---
+++ metalogue/src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(VueRouter)
+Vue.use(BootstrapVue)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
